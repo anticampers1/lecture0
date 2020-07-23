82dd1419e0133c7d69cddb28335199542af45e29
anticampers1 <kevincastroj50@gmail.com>
2020-07-23 10:19:30 -0600

    Signed-off-by: anticampers1 <kevincastroj50@gmail.com>

---
+++ Python/loops.py
@@ -0,0 +1,10 @@
+##for i in [0,1,2,3,4,5]:
+##    print(i)
+for i in range(6):
+    print(i)
+names=["A","B","C","D","E"]
+for name in names:
+    print(name)
+名前="tanaka"
+for character in 名前:
+    print(character)
