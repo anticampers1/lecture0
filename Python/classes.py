d541b628f455226bad18d08d10864f95c4df20a9
anticampers1 <kevincastroj50@gmail.com>
2020-07-29 16:32:06 -0600

    Signed-off-by: anticampers1 <kevincastroj50@gmail.com>

--- Python/classes.py
+++ Python/classes.py
@@ -4,5 +4,30 @@ class Point():
         self.y=y
 
 p=Point(2,8)
-print(p.x)
-print(p.y)
+##print(p.x)
+##print(p.y)
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity=capacity
+        self.passangers=[]
+
+    def add_passanger(self, name):
+        if not self.open_seats():
+            return False
+        self.passangers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity-len(self.passangers)
+
+Flight=Flight(3)
+
+people=['Harry','Ron','Hermione','Ginny']
+for person in people:
+    success=Flight.add_passanger(person)
+    if success:
+        print(f'Added {person} to flight successfully.')
+    else:
+        print(f'No available seats for {person}.')
+        
