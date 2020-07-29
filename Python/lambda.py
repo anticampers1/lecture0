c7f257dcb4a8c38ecbe58a1a395f944e27cffddc
anticampers1 <kevincastroj50@gmail.com>
2020-07-29 16:46:17 -0600

    Signed-off-by: anticampers1 <kevincastroj50@gmail.com>

--- Python/lambda.py
+++ Python/lambda.py
@@ -1,6 +1,9 @@
 people=[
-    {'name':'Harry','House':'Gryffindor'},
-    {'name':'Cho','House':'Ravenclaw'},
+    {'name':'Harry','house':'Gryffindor'},
+    {'name':'Cho','house':'Ravenclaw'},
     {'name':'Draco','house':'slytherin'}
 ]
+
+people.sort(key=lambda person:person['name'])
+
 print(people)
